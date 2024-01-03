bb6b828bf4d81acff090f8cd2180c30843568d21
Elise Segal <elisesegal@gmail.com>
2024-01-03 16:59:25 -0500

    Hey matt!

--- 4327 2024 Test Code/src/main/java/frc/robot/RobotContainer.java
+++ 4327 2024 Test Code/src/main/java/frc/robot/RobotContainer.java
@@ -98,7 +98,7 @@ public class RobotContainer {
         s_Swerve.setDefaultCommand(
             new TeleopSwerve(
                 s_Swerve, 
-                () -> -driver.getRawAxis(translationAxis), 
+                () -> driver.getRawAxis(translationAxis), 
                 () -> -driver.getRawAxis(strafeAxis), 
                 () -> -driver.getRawAxis(rotationAxis), 
                 () -> robotCentric.getAsBoolean(),
